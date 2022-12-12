9940ef29541ef3e388002f38d50c42a3aa06feb4
林政浩 <1392872316@qq.com>
2022-12-12 22:44:39 +0800

    添加项目文件。

---
+++ Test_P174.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+//1.
+	//char ch[26] = {0};
+	//int i;
+
+	//for (i = 0; i < 26; i++) {
+	//	ch[i] = 'a' + i;
+	//	printf("%2c", ch[i]);
+	//}
+
+//2.
+	//int i, j;
+
+	//for (i = 0; i < 5; i++) {
+	//	for (j = 0; j <= i; j++) {
+	//		printf("$");
+	//	}
+	//	printf("\n");
+	//}
+
+//3.
+
+
+	return 0;
+}
