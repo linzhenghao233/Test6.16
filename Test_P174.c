41fe3121a9ae325f6479dcce10d6a680bce71484
林政浩 <1392872316@qq.com>
2022-12-13 23:39:00 +0800

    1213

--- Test_P174.c
+++ Test_P174.c
@@ -21,7 +21,44 @@ int main(void) {
 	//}
 
 //3.
+	//int i, j;
 
+	//for (i = 0; i < 6; i++) {
+	//	for (j = 0; j <= i; j++) {
+	//		printf("%c", 'F' - j);
+	//	}
+	//	printf("\n");
+	//}
+
+//4.
+	//int i, j, k = 0;
+
+	//for (i = 0; i < 6; i++) {
+	//	for (j = 0; j <= i; j++, k++) {
+	//		printf("%c", 'A' + k);
+	//	}
+	//	printf("\n");
+	//}
+
+//5.
+	int i, j, k, m;
+	char ch;
+
+	printf("请输入一个大写字母：_\b");
+	scanf_s("%c", &ch, 1);
+
+	for (i = 0; i <= ch - 65; i++) {
+		for (j = 0; j < ch - 66 - i; j++) {
+			printf(" ");
+		}
+		for (k = 0; k <= i; k++) {
+			printf("%c", 'A' + k);
+		}
+		for (m = 0; m < i; m++) {
+			printf("%c", 64 + i - m);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
