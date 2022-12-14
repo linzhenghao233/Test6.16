8be1e16a01c7092636775973f016c9cc30db7db4
林政浩 <1392872316@qq.com>
2022-12-14 23:42:35 +0800

    1214

--- Test_P174.c
+++ Test_P174.c
@@ -41,23 +41,45 @@ int main(void) {
 	//}
 
 //5.
-	int i, j, k, m;
-	char ch;
+	//int i, j, k, m;
+	//char ch;
 
-	printf("请输入一个大写字母：_\b");
-	scanf_s("%c", &ch, 1);
+	//printf("请输入一个大写字母：_\b");
+	//scanf_s("%c", &ch, 1);
 
-	for (i = 'A'; i <= ch; i++) {
-		for (j = 0; j < ch - i; j++) {
-			printf(" ");
-		}
-		for (k = 'A'; k <= i; k++) {
-			printf("%c", k);
-		}
-		for (m = 1; m <= k - 'B'; m++) {
-			printf("%c", k - m - 1);
-		}
-		printf("\n");
+	//for (i = 'A'; i <= ch; i++) {
+	//	for (j = 0; j < ch - i; j++) {
+	//		printf(" ");
+	//	}
+	//	for (k = 'A'; k <= i; k++) {
+	//		printf("%c", k);
+	//	}
+	//	for (m = 1; m <= k - 'B'; m++) {
+	//		printf("%c", k - m - 1);
+	//	}
+	//	printf("\n");
+	//}
+
+//6.
+	//int i, j;
+
+	//printf("输入一个整数：");
+	//scanf_s("%d", &i);
+	//printf("再输入一个更大的整数：");
+	//scanf_s("%d", &j);
+
+	//for (; i <= j; i++) {
+	//	printf("整数：%d，平方：%d，立方：%d\n", i, i * i, i * i * i);
+	//}
+
+//7.
+	int i;
+	char ch[20] = { 0 };
+
+	printf("请输入一个单词：");
+	scanf_s("%s", &ch, 20);
+	for (i = strlen(ch); i >= 0; i--) {
+		printf("%c", ch[i]);
 	}
 
 	return 0;
